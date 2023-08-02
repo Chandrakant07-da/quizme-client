3ca5e239e58e743a27a68b07462899c027426216
chandrakant <chandra.kant@kellton.com>
2023-08-02 18:21:47 +0530

    Updated rendering process

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -6,7 +6,7 @@ type Props = {
 const Button = ({ text, onClick }: Props) => {
   return (
     <button
-      className='bg-[#9F50AC] select-none font-bold h-[45px] min-w-[120px] rounded-[10px] text-white hover:bg-sky-700 whitespace-normal overflow-hidden'
+      className='bg-[#9F50AC] select-none font-bold h-[45px] min-w-[120px] rounded-[10px] text-white hover:bg-sky-700 whitespace-normal overflow-hidden p-2'
       onClick={onClick}
     >
       {text}
